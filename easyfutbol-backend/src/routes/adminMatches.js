8bf6c36b2fee9250d95280028e4350806334d8b1
Roberto Merchan <robertomerchan@MacBook-Air.local>
2026-03-25 00:12:24 +0100

    update backend

--- easyfutbol-backend/src/routes/adminMatches.js
+++ easyfutbol-backend/src/routes/adminMatches.js
@@ -1,6 +1,6 @@
 
 import express from 'express';
-import db from '../db.js';
+import db from '../config/db.js';
 import verifyToken from '../middleware/verifyToken.js';
 
 const router = express.Router();
