a6e7782b7917bc525211b21e3e4f7e1db9c00d65
Roberto Merchan <robertomerchan@MacBook-Air.local>
2026-04-29 14:05:19 +0200

    update backend

--- easyfutbol-backend/src/routes/appConfigRoutes.js
+++ easyfutbol-backend/src/routes/appConfigRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/min-version', (req, res) => {
   res.json({
-    minVersion: '1.0.15',
+    minVersion: '1.0.16',
     androidUrl: 'https://play.google.com/store/apps/details?id=es.easyfutbol.app&pcampaignid=web_share',
     iosUrl: 'https://apps.apple.com/es/app/easyfutbol-app/id6758757708',
     message: 'Hay una nueva versión disponible. Actualiza la app para continuar.'
